1c10bc95106c4f3f57e35a10aeebc9d82df58906
nomisrevol <vdn1999bxvp@gmail.com>
2020-08-16 23:14:13 +0700

    Add footprint for user, problem and contest

---
+++ gondolin/contest.py
@@ -0,0 +1,18 @@
+import sys
+from typing import List
+
+
+def get_contest_submission(contest_id: int) -> List[str]:
+    """
+    :param contest_id: contest id
+    :return: a list of path to submission
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ gondolin/user.py
@@ -0,0 +1,37 @@
+import sys
+from typing import List
+
+
+def get_participants_in_contest(contest_id: int) -> List[str]:
+    """
+    :param contest_id:
+    :return: a list of username of users in provided contest
+    """
+    pass
+
+
+def get_all_users() -> List[str]:
+    """
+    :return: a list of all users's username in cms system
+    """
+
+
+def remove_all_users() -> None:
+    """
+    this function remove all users in cms system
+    """
+
+
+def remove_list_user(users: List[str]) -> None:
+    """
+    :param users: a list of username
+    :return: nothing, just remove users in list from system
+    """
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
